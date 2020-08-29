76e9658b599e7e92d6b8fbc10cef6a698ee5198b
stevetidy <stevetidy@gmail.com>
2020-08-29 17:03:20 -0400

    Added SCSS files and SASS functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Login from './components/Login';
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Login />
   );
 }
 
