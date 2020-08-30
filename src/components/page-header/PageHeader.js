df092f967e92a03fc016020a17447b57b8f86701
Steve Tidy <stevetidy@MacBook-Pro.local>
2020-08-30 13:44:23 -0400

    Changed class to className

--- src/components/page-header/PageHeader.js
+++ src/components/page-header/PageHeader.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const PageHeader = ({pageHeaderTitle, pageHeaderCopy}) => {
+const PageHeader = ({ pageHeaderTitle, pageHeaderCopy }) => {
   return (
-    <div class="page-header">
-      <h1 class="page-header__title">{pageHeaderTitle}</h1>
-      <p class="page-header__copy">{pageHeaderCopy}</p>
+    <div className="page-header">
+      <h1 className="page-header__title">{pageHeaderTitle}</h1>
+      <p className="page-header__copy">{pageHeaderCopy}</p>
     </div>
   )
 };
