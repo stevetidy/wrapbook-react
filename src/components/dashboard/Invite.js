3275951fee338e9462f2714bde6405a73d495962
stevetidy <stevetidy@gmail.com>
2020-08-31 18:24:06 -0400

    Update Invite.js
    Modified Copy to clipboard script.

--- src/components/dashboard/Invite.js
+++ src/components/dashboard/Invite.js
@@ -3,16 +3,19 @@ import React, { useRef, useState } from 'react';
 const Invite = () => {
   // copyToClipboard function taken from:
   // https://stackoverflow.com/a/42844911
-  const [copySuccess, setCopySuccess] = useState('');
+  const [copySuccess, setCopySuccess] = useState(false);
   const textAreaRef = useRef(null);
+  const linkCopySuccessTxt = 'Link Copied!';
+  const linkCopyCtaTxtDesktop = 'Copy to clipboard';
+  const linkCopyCtaTxtMobile = 'Invite someone to Wrapbook';
 
   function copyToClipboard(e) {
     textAreaRef.current.select();
     document.execCommand('copy');
     e.target.focus();
-    setCopySuccess('...Copied!');
+    setCopySuccess(true);
     setTimeout(() => {
-      setCopySuccess('');
+      setCopySuccess(false);
     }, 2500);
   };
 
@@ -25,9 +28,9 @@ const Invite = () => {
         <h2 className="invite__title">Invite people to this project</h2>
         <p className="invite__copy">Share the following link with your team to have them sign up and send you their invoices.</p>
         <p><span className="invite__link">https://wrapbook.com/i/j93kda92</span></p>
-        <button onClick={copyToClipboard} className="button button--copy">Copy to clipboard{copySuccess}</button>
+        <button onClick={copyToClipboard} className="button button--copy">{copySuccess ? linkCopySuccessTxt : linkCopyCtaTxtDesktop}</button>
       </div>
-      <button onClick={copyToClipboard} className="button button--copy button--invite-mobile">Invite someone to Wrapbook{copySuccess}</button>
+      <button onClick={copyToClipboard} className="button button--copy button--invite-mobile">{copySuccess ? linkCopySuccessTxt : linkCopyCtaTxtMobile}</button>
       <form className="visuallyhidden">
         <label htmlFor="invite-link">Invite Link</label>
         <textarea
