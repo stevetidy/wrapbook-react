5fd4aa47f18519fc830220aa03276f728be346a6
stevetidy <stevetidy@gmail.com>
2020-08-31 15:48:22 -0400

    Style tweaks

--- src/components/dashboard/Invite.js
+++ src/components/dashboard/Invite.js
@@ -24,11 +24,12 @@ const Invite = () => {
         </picture>
         <h2 className="invite__title">Invite people to this project</h2>
         <p className="invite__copy">Share the following link with your team to have them sign up and send you their invoices.</p>
-        <p className="invite__link">https://wrapbook.com/i/j93kda92</p>
+        <p><span className="invite__link">https://wrapbook.com/i/j93kda92</span></p>
         <button onClick={copyToClipboard} className="button button--copy">Copy to clipboard{copySuccess}</button>
       </div>
       <button onClick={copyToClipboard} className="button button--copy button--invite-mobile">Invite someone to Wrapbook{copySuccess}</button>
       <form className="visuallyhidden">
+        <label htmlFor="invite-link">Invite Link</label>
         <textarea
           readOnly
           name="invite-link"
